c005d1f836877345cb1bb7e408b8a6bf1e2c8f0f
lukaskiermeyer <80913379+lukaskiermeyer@users.noreply.github.com>
2025-03-05 20:40:05 +0100

    layout update

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Website geladen');
     // Hier kannst du deine Animationen oder weitere JavaScript-Funktionen hinzufügen
 });
+
+function toggleMenu() {
+    document.querySelector(".nav-menu").classList.toggle("active");
+}
+
