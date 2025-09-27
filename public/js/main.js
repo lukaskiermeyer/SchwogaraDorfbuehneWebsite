d1fef5ad240f2a2e9bccd726c02a151f440e28c8
lukaskiermeyer <80913379+lukaskiermeyer@users.noreply.github.com>
2025-09-27 14:31:15 +0200

    .

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,6 @@
 // public/js/main.js
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Website geladen');
-    // Hier kannst du deine Animationen oder weitere JavaScript-Funktionen hinzufügen
+
 });
 
 function toggleMenu() {
